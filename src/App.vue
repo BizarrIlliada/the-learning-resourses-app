f9cf2b9a65da2402da4cf72c534433d995894a3c
BizarrIlliada <kazkoviy@icloud.com>
2023-08-03 00:30:18 +0300

    basic mockup

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <TheHeader />
+  <ul>
+    <LearningResource
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :link="resource.link"
+      :title="resource.title"
+      :description="resource.description"
+    />
+  </ul>
+</template>
+
+<script>
+  import TheHeader from './components/layout/TheHeader.vue';
+  import LearningResource from './components/learning-resources/LearningResource.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      TheHeader,
+      LearningResource,
+    },
+
+    data() {
+      return {
+        storedResources: [
+          {
+            id: '1',
+            title: 'Official Guide',
+            description: 'The official Vue.js documentation.',
+            link: 'https://vuejs.org',
+          },
+          {
+            id: '2',
+            title: 'Google',
+            description: 'Learn to Google',
+            link: 'https://google.org',
+          },
+        ],
+      }
+    },
+  }
+</script>
+
+<style>
+</style>
---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,38 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>
+          Delete
+        </button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link">
+        View resource
+      </a>
+    </nav>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'LearningResource',
+    props: {
+      link: {
+        type: String,
+        require: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+    },
+  }
+</script>
